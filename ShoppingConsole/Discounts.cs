04032b845776d4f1a8b99c23259e157d47a80973
Deadmetal85 <whyubotherme@gmail.com>
2015-10-16 14:51:50 +0800

    UI

---
+++ ShoppingConsole/Discounts.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiscountEngine
+{
+    public class Discounts
+    {
+        public double priceTotal;
+
+        public double GetDiscounts(Product[] product)
+        {
+
+            product = new Product[1];
+            product[0].sku = "100.1";
+            product[0].price = 120.0;
+            product[1].sku = "100.1";
+            product[1].price = 100.0;
+
+            if (product.Count() > 1)
+            {
+                for (int i = 0; i < product.Count() - 1; i++)
+                {
+                    if (i == 0)
+                    {
+                        priceTotal += product[i].price;
+                    }
+                    else if (i > 0 && product[i - 1].sku.Equals(product[i].sku))
+                    {
+                        priceTotal += (product[i].price * 0.7);
+                    }
+                    else if (i > 0 && !(product[i - 1].sku.Equals(product[i].sku)))
+                    {
+                        priceTotal += (product[i].price * 0.5);
+                    }
+                }
+            }
+            else
+            {
+                priceTotal = product[0].price;
+            }
+
+            return priceTotal;
+        }
+    }
+}
---
+++ ShoppingConsole/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiscountEngine
+{
+    public class Product
+    {
+        public string sku { get; set; }
+        public double price { get; set; }
+    }
+}
