b649d5ce4d3ef80891d981c41e561c9e15a678b6
Developer <test@test.com>
2015-10-16 14:17:18 +0800

    Shopping Cart Version 1

---
+++ ShoppingConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] items = new string[,] { { "Eggs", "10" }, { "Bread", "15" } };
+            string input;
+            int price = 0;
+            int quantity;
+            int index;
+            
+            for (int i=0;i<items)
+
+            Console.WriteLine(items);
+
+            do
+            {
+                input = Console.ReadLine();
+                Console.WriteLine("Enter quantity: ");
+                index = Convert.ToInt32(input) + 1;
+                quantity = Convert.ToInt32(Console.ReadLine());
+                price += quantity * Convert.ToInt32(items[index, 1]); // accessing price of item
+
+            } while (input != "S");
+
+            Console.WriteLine("Your total: " + price);
+        }
+    }
+}
